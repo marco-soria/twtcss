3ac22b73691085f891356593692baaa328fe6fdd
marco-soria <marco.soriaa7@gmail.com>
2024-06-29 15:03:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <p className="text-7xl text-red-500">Hello World</p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
